6a8c7b149a9fc4ebdf7ae48851f3cd61f6cc7e1e
milan <milanwithana97@gmail.com>
2022-09-04 18:09:33 +0530

    4 faq section

---
+++ milanme/src/components/FaqSection.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const FaqSection = () =>{
+    return(
+   <div className="faq">
+    <h2>Any Questions <span>FAQ</span></h2>
+    <div className="question">
+        <h4>How Do I start?</h4>
+        <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+            </p>
+        </div>
+    </div>
+    <div className="question">
+        <h4>What is your Job possition?</h4>
+        <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+            </p>
+        </div>
+    </div>
+    <div className="question">
+        <h4>job experience?</h4>
+        <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+            </p>
+        </div>
+    </div>
+    <div className="question">
+        <h4>Technology used?</h4>
+        <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+            </p>
+        </div>
+    </div>
+   </div>
+    )
+}
+
+export default FaqSection;
--- milanme/src/pages/AboutUs.js
+++ milanme/src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //page component
 import AboutSection  from "../components/AboutSection";
 import QualificationSection from "../components/QualificationSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs =() =>{
     return (
     <>
   <AboutSection />
   <QualificationSection />
+  <FaqSection />
   </>
     );
 }
