b3d0fdb251d9d8626fe428cc9cdef1ecd47c9954
milan <milanwithana97@gmail.com>
2022-09-10 16:35:03 +0530

    7 faq styled

--- milanme/src/components/FaqSection.js
+++ milanme/src/components/FaqSection.js
@@ -1,47 +1,84 @@
 import React from "react";
+import styled from "styled-components";
+import { About } from "../style";
+
 
 const FaqSection = () =>{
     return(
-   <div className="faq">
+   <Faq>
     <h2>Any Questions <span>FAQ</span></h2>
     <div className="question">
         <h4>How Do I start?</h4>
         <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente
             </p>
         </div>
+        <div className="faq-line"></div>
+
     </div>
     <div className="question">
         <h4>What is your Job possition?</h4>
         <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
-            </p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente         
+                    </p>
         </div>
+        <div className="faq-line"></div>
     </div>
     <div className="question">
         <h4>job experience?</h4>
         <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente 
             </p>
         </div>
     </div>
+    <div className="faq-line"></div>
+
     <div className="question">
         <h4>Technology used?</h4>
         <div className="answer">
             <p>Lorem ipsum dolor sit amet.</p>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente beatae alias magni dignissimos, atque odio corporis velit temporibus quam error dolore quis aut tempora porro deserunt id.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, repellat quo sapiente 
             </p>
         </div>
     </div>
-   </div>
+    <div className="faq-line"></div>
+
+   </Faq>
     )
 }
+
+const Faq = styled(About)`
+       display: block;
+       span{
+        display: block;
+       }
+       h2{
+        padding-bottom: 2rem;
+        font-weight: lighter;
+       }
+       .faq-line{
+        background: #cccccc;
+        height:0.2rem;
+        margin:2rem 0rem;
+        width: 100%;
+       }
+       .question{
+        padding: 3rem 0rem;
+        cursor:pointer ;
+       }
+       .answer{
+        padding: 2rem 0rem;
+        p{
+            padding: 1rem 0rem;
+        }
+       }
+`;
 
 export default FaqSection;
--- milanme/src/components/GlobalStyle.js
+++ milanme/src/components/GlobalStyle.js
@@ -39,6 +39,7 @@ h3{
 h4{
     font-weight: bold;
     color: #23d997;
+    font-size: 2rem;
 }
 a{
     font-size: 1.1rem;
