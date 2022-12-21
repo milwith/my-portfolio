0f8606266def176aa97f66ce15c6a063688c698b
milan <milanwithana97@gmail.com>
2022-12-21 20:55:43 +0530

    setup by

--- milanme/src/App.js
+++ milanme/src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import MyWork from "./pages/MyWork";
+//router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Swith --> Routes
+
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-     <AboutUs />
+
+      <Router>
+        <Nav />
+        <div>
+          <Routes>
+            <Route exact path="/" element={<AboutUs />} />
+            <Route exact path="/work" element={<MyWork />} />
+            <Route exact path="/contact" element={<ContactUs />} />
+
+          </Routes>
+        </div>
+
+
+      </Router>
+
+
     </div>
   );
 }
---
+++ milanme/src/pages/MyWork.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyWork =() =>{
+
+    return(
+     <div>
+        <h1>our work</h1>
+     </div>
+    );
+};
+
+export default MyWork;
